c427ff842dbbd1efaf2d7b1664297ba4516aa119
Pigrabbit <donghyuk1003@gmail.com>
2020-05-14 21:24:39 +0900

    Updates

---
+++ precache-manifest.7774537f65c06806019bcbb91cf13e62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a804c36b1edf2951ace4ba5f90dee46",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "70879748d4129b4282b5",
+    "url": "/movie-app/static/css/main.b59681af.chunk.css"
+  },
+  {
+    "revision": "f2ae0e016303c0525327",
+    "url": "/movie-app/static/js/2.462ac3f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app/static/js/2.462ac3f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70879748d4129b4282b5",
+    "url": "/movie-app/static/js/main.75bccd0f.chunk.js"
+  },
+  {
+    "revision": "eb2afba229eb5426c33e",
+    "url": "/movie-app/static/js/runtime-main.6de98e8b.js"
+  }
+]);
